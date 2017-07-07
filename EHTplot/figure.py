dc89b7be6668038ce40a0938836eeb198e32ac77
Lia Medeiros <lia.cf.medeiros@gmail.com>
2017-07-07 14:36:53 -0400

    created Figure class

--- EHTplot/figure.py
+++ EHTplot/figure.py
@@ -15,5 +15,24 @@
 # You should have received a copy of the GNU General Public License                                           
 # along with EHTplot.  If not, see <http://www.gnu.org/licenses/>.  
 
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm, colors, rcParams
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.colors import LogNorm
+from matplotlib.ticker import MultipleLocator
+
+
 class Figure:
-    pass
+    def __init__(self, subplots=(1,1), size=None, font=10, sharex=False, sharey=False):
+        '''
+        subplots is (row, column) but size is (x,y)
+        '''
+        
+        textwidth=7.1
+        columnwidth=3.39375
+        if size is None:
+            w = columnwidth if subplots[1]==1 else textwidth
+            size = (w, w/subplots[1]*subplots[0])
+        self.fig, self.axs = plt.subplots(subplots[0], subplots[1], sharex=sharex, sharey=sharey, figsize=size)      
