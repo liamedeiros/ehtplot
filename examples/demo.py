ec9ad8157953ba0358b86b31f5793a1618e21960
Chi-kwan Chan <chanc@email.arizona.edu>
2018-11-27 23:15:20 -0700

    Make the demo more generic

--- examples/demo.py
+++ examples/demo.py
@@ -2,11 +2,11 @@
 
 import ehtplot as ep
 
-img = ep.open("sample")
-pnl = ep.Panel(image=img)
-fig = ep.Figure(pnl)
+img = ep.Panel(image=ep.open("sample"))
+row = ep.Panel([img, img, img])
+fig = ep.Figure([row, row], inrow=False)
 
-#fig.save("demo-seaborn.png", style='seaborn')
-#fig.save("demo-ggplot.png",  style='ggplot')
+fig.save("demo-seaborn.png", style='seaborn')
+fig.save("demo-ggplot.png",  style='ggplot')
 fig.save("demo-ehtplot.png", style='ehtplot')
-#fig.save("demo.png")
+fig.save("demo.png")
