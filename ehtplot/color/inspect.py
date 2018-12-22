b283db5ac04884b6d17264aeb893679ad55558e1
Chi-kwan Chan <chanc@email.arizona.edu>
2018-12-22 09:48:21 -0700

    Implement a pyramid function in "color/inspect.py"

---
+++ ehtplot/color/inspect.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2018 Chi-kwan Chan
+# Copyright (C) 2018 Steward Observatory
+#
+# This file is part of ehtplot.
+#
+# ehtplot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ehtplot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ehtplot.  If not, see <http://www.gnu.org/licenses/>.
+
+import numpy as np
+
+def pyramid(N=513):
+    s    = np.linspace(-1.0, 1.0, N)
+    x, y = np.meshgrid(s, s)
+    return 1.0 - np.maximum(abs(x), abs(y))
